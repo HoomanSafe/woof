c28dd679958ac4a81037bcc6617670c3cb6afbe9
Hooman Dev <98424162+HoomanSafe@users.noreply.github.com>
2024-11-12 11:47:45 +0800

    Update About.jsx

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Logo2 from "../assets/logo2.png";
+import Logo2 from "../assets/logo3.png";
 
 const About = () => {
   return (
@@ -8,25 +8,32 @@ const About = () => {
         <img src={Logo2} alt="" />
         <article className="flex  z-10 flex-col gap-5">
           <h1 className="text-white text-5xl font-squishyBlue ">
-            About Droppy
+            About WoofHopsi
           </h1>
           <p className=" text-white text-base font-normal leading-loose">
-            Prepare yourself for a crypto uprising like never seen before!
-            Droppy has arrived to disrupt the scene with its 1% tax and a grand
-            total of 69,000,000 tokens. But what makes Droppy truly exceptional?
-            We bring the party! Come along for uproarious escapades, exclusive
-            gatherings, and a community that's cooler than an arctic breeze.
-            Gear up to dive into Droppy and encounter a whole new world of
-            crypto excitement. Let's make the blockchain groove!
+            WoofHopsi is a playful and innovative meme project featuring an adorable husky character designed to bring smiles and joy to the digital world. WoofHopsi captures the essence of fun and positivity, combining catchy visuals with relatable,
+            humorous content.
           </p>
           <a
-            href=""
+            href="https://t.me/WoofHopsi"
             className="w-[267px] h-[74px] pl-px pb-1 bg-lime-400 justify-center items-center inline-flex"
           >
             <div className="grow shrink basis-0 self-stretch p-[5px] bg-white justify-center items-center inline-flex">
               <div className="grow shrink basis-0 self-stretch px-8 py-4 bg-red-600 shadow justify-center items-center gap-2.5 inline-flex">
                 <div className="text-white text-xl font-normal leading-7">
                   JOIN TELEGRAM
+                </div>
+              </div>
+            </div>
+          </a>
+           <a
+            href="https://woofhopsi.gitbook.io/woofhopsi-docs"
+            className="w-[267px] h-[74px] pl-px pb-1 bg-lime-400 justify-center items-center inline-flex"
+          >
+            <div className="grow shrink basis-0 self-stretch p-[5px] bg-white justify-center items-center inline-flex">
+              <div className="grow shrink basis-0 self-stretch px-8 py-4 bg-red-600 shadow justify-center items-center gap-2.5 inline-flex">
+                <div className="text-white text-xl font-normal leading-7">
+                  DOCS
                 </div>
               </div>
             </div>
